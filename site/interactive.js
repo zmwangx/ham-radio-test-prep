01a99dae2cb0b25e1f3bb10ddb4a5da09bc7b9b1
Zhiming Wang <zmwangx@gmail.com>
2017-06-03 19:14:08 -0400

    Add a brief status popup when switching display modes
    The display may not be obvious, unlike the state of show-answers.

--- site/interactive.js
+++ site/interactive.js
@@ -4,17 +4,25 @@ $(function () {
   var $window = $(window)
   var $document = $(document)
   var $body = $('body')
+  var $status = $('#status')
 
   var cycleDoneDisplayModes = function () {
+    var status
     if ($body.hasClass('done-show-titles')) {
       $body.addClass('done-hide').removeClass('done-show-titles')
+      status = 'Hidden'
     } else if ($body.hasClass('done-hide')) {
       $body.addClass('done-show').removeClass('done-hide')
+      status = 'Shown'
     } else if ($body.hasClass('done-show')) {
       $body.addClass('done-show-titles').removeClass('done-show')
+      status = 'Question bodies hidden'
     } else {
       console.warn('body has none of the done-show-titles, done-hide, and done-show classes.')
+      return
     }
+    $status.clearQueue()
+    $status.text(status).show(200).delay(500).hide(200)
   }
 
   var saveState = function () {
