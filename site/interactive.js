b675e5a33d96e656d167a69cd5dfb759a4943cc5
Zhiming Wang <zmwangx@gmail.com>
2017-06-02 23:17:21 -0400

    interactive.js: use jQuery's .toggleClass()

--- site/interactive.js
+++ site/interactive.js
@@ -8,11 +8,7 @@ $(function () {
     var key = String.fromCharCode(event.which)
     switch (key) {
       case 'a':
-        if ($body.hasClass('show-answers')) {
-          $body.removeClass('show-answers')
-        } else {
-          $body.addClass('show-answers')
-        }
+        $body.toggleClass('show-answers')
         break
       default:
         break
