dd1e015b8553455955fd8dc884069be91e183b42
Zhiming Wang <zmwangx@gmail.com>
2017-06-02 23:51:51 -0400

    Implement clicking to mark questions as done
    "Done" questions have three display modes that can be toggled with the 's' key:
    show titles only (default), hide completely, and show in full. Regardless, they
    always have green check marks next to their titles and they are always dimmed.
    
    Yet another build in this commit, too.

--- site/interactive.js
+++ site/interactive.js
@@ -4,14 +4,34 @@ $(function () {
   var $document = $(document)
   var $body = $('body')
 
+  var cycleDoneDisplayModes = function () {
+    if ($body.hasClass('done-show-titles')) {
+      $body.addClass('done-hide').removeClass('done-show-titles')
+    } else if ($body.hasClass('done-hide')) {
+      $body.addClass('done-show').removeClass('done-hide')
+    } else if ($body.hasClass('done-show')) {
+      $body.addClass('done-show-titles').removeClass('done-show')
+    } else {
+      console.warn('body has none of the done-show-titles, done-hide, and done-show classes.')
+    }
+  }
+
   $document.on('keypress', function (event) {
     var key = String.fromCharCode(event.which)
     switch (key) {
       case 'a':
         $body.toggleClass('show-answers')
         break
+      case 's':
+        cycleDoneDisplayModes()
+        break
       default:
         break
     }
+    return false
+  })
+
+  $('.question .title').click(function () {
+    $(this).parent().toggleClass('done')
   })
 })
